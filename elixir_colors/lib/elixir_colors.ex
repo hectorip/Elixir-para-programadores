0d029992dd035ab1cf5bf0a2996a2ac5ee3380b0
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-05-26 20:36:25 -0600

    Cambiando explicación de funciones nombradas a modelo de MIX

--- elixir_colors/lib/elixir_colors.ex
+++ elixir_colors/lib/elixir_colors.ex
@@ -1,11 +1,18 @@
+# En este script vamos a explicar la sintaxis de los módulos
+# y las funciones nombradas en Elixir. Vamos a ver todos los detalles
+# que las hacen diferentes de la mayoría de los lenguajes
+# más populares.
+
 defmodule ElixirColors do
   @moduledoc """
-  Documentation for `ElixirColors`.
+  Esta es la documentación del módulo `ElixirColors`. Se usa para proveer
+  infortmación en general sobre el módulo. Algo que no me gusta
+  es que esta información se provee _después_ de la apertura del módulo
+  y como verás más adelante, la información
   """
 
   @doc """
-  Hello world.
-
+  Esta es la documentación de la función nombrada. 
   ## Examples
 
       iex> ElixirColors.hello()
