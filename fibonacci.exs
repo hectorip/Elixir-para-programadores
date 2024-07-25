db0ee12ec99010c6cdacdfd1b16c575b6cd9e2e6
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2024-07-24 23:53:16 -0600

    Explicación de fibonacci

--- fibonacci.exs
+++ fibonacci.exs
@@ -1,4 +1,8 @@
+# Implementemos de manera sencilla Fibonacci, puedes ver como casi
+# es directamente la definición matemática de la sucesión.
+
 defmodule FibonacciNaive do
   def fib(n) when n <= 1, do: n
+  # Esta implementación es ineficiente porque calcula el mismo número un montón de veces
   def fib(n), do: fib(n - 1) + fib(n - 2)
 end
